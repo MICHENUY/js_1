e9a84194a37a2136c2b8f50d0736ad0fcb378597
MICHENUY <adriytkachuk162@gmail.com>
2022-11-04 19:27:09 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+let hello = "hello";
+console.log(hello);
+
+let owu = "owu";
+console.log(owu);
+
+let com = "com";
+console.log(com);
+
+let ua = "ua";
+console.log(ua);
+
+let one = 1;
+console.log(one);
+
+let ten = 10;
+console.log(ten);
+
+let minus = -999;
+console.log(minus);
+
+let first = 123;
+console.log(first);
+
+let pi= 3.14;
+console.log(pi);
+
+let twenty_sewen = 2.7;
+console.log(twenty_sewen)
+
+let sixten = 16;
+console.log(sixten);
+
+let tr = true;
+console.log(tr);
+
+let fuck = false;
+console.log(fuck);
+
+let firstName = "Ткачук"
+let midleName = "Андрій"
+let lastName = "Сергійович"
+let person = firstName + "" +  midleName+ "" + lastName;
+console.log(person);
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let year = " 20 років";
+alert(person + year);
+prompt(person + year);
+confirm(person + year);
+co
+
